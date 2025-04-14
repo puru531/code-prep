ee80bcc025896a189e89652f73143e37073e3634
Purushottam <purushottam1619@gmail.com>
2025-04-15 01:12:05 +0530

    prod url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api'
+  production: true,
+  // production: false,
+  apiUrl: 'https://code-prep-purushottam-kumars-projects.vercel.app/api'
+  // apiUrl: 'http://localhost:5000/api'
 };
